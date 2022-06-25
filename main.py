76ca8d8fd0cd2d36635b4d99fff685cadbbd9546
fabi.s <trapffm@web.de>
2022-06-25 12:49:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from datetime import datetime
 URLS = ["https://lz4.overpass-api.de/api/interpreter", "https://z.overpass-api.de/api/interpreter", "https://overpass.kumi.systems/api/interpreter", "https://overpass.openstreetmap.ru/api/interpreter", "https://overpass.openstreetmap.fr/api/interpreter"]
 DATASTRING = '?data=[out:json];(node[%27amenity%27=%27bench%27](50.22364307664712,8.449115594560567,50.24036141038248,8.46567765838438);node[%27leisure%27=%27picnic_table%27](50.22364307664712,8.449115594560567,50.24036141038248,8.46567765838438););out%20body;'
 
-
 print("Script started..")
 while True:
     for URL in URLS:
